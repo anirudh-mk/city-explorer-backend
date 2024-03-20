0feb7dcc1394acad92bc01b7a62181ee7708ce56
anirudh-mk <its.me.anirudh.mk@gmail.com>
2024-03-20 15:42:28 +0530

    feat(usermodel added)

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_superuser = None
+    is_staff = None
+    last_login = None
+    id = models.CharField(primary_key=True, max_length=36, default=uuid.uuid4())
